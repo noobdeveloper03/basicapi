ab929b30dd25361db38d65979f32ca3614b94b63
noobdeveloper03 <noob.developer03@gmail.com>
2024-02-04 15:45:33 +0800

    Added custom logger

--- src/main/java/com/example/basicapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/basicapi/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.example.basicapi.exception.UserException;
 import com.example.basicapi.models.UserRequest;
 import com.example.basicapi.service.UserService;
 import com.example.basicapi.util.UserConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
@@ -25,7 +29,9 @@ public class UserServiceImpl implements UserService {
     public void createUser(UserRequest request) {
         //Todo: Add Error Handling for existing username or email validation
         User user = UserConverter.convertUser(request);
+        logger.info("Validating User {}",user);
         if(userDao.doesExist(user)) {
+            logger.error("Username: {} or email: {} already exist",user.getUsername(),user.getEmail());
             throw new UserException("Username or email Already Exist");
         }
         userDao.createUser(UserConverter.convertUser(request));
